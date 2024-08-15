52a47f6d13b1b523ac1b83f376dc70f3a5567580
PLURninjaDev <dr.schaphorst@outlook.com>
2024-08-15 14:26:44 -0400

    Import Temp Assets

--- scripts/hello_world.gd
+++ scripts/hello_world.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
+@onready var grid_map = $GridMap
+
+var blocks_grass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	blocks_grass = grid_map.get_used_cells_by_item()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if Input.is_action_just_pressed("click"):
+		event.get_
